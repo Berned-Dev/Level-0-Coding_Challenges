00b15ffe714e3b8f96e18d919511db00fd424a54
Berned <51479929+Berned-Dev@users.noreply.github.com>
2021-01-22 11:25:57 +0200

    Update task_10.java
    I have removed a piece of code that made this code redundant.

--- task_10.java
+++ task_10.java
@@ -5,8 +5,6 @@ public class task_10 {
         char[] stringOneArray = stringOne.toCharArray();
         char[] stringTwoArray = stringTwo.toCharArray();
 
-        if (stringOne.length() > stringTwo.length()){
-
             for (int j = 0; j < stringOne.length(); j++) {          //Loop uses first string length to iterate throught the charecters of first string.
                 for (int i = 0; i < stringTwo.length(); i++) {      //loop uses second string length to iterate throught the charecters of second string.
                     if (stringOneArray[j] == stringTwoArray[i]) {
@@ -16,16 +14,6 @@ public class task_10 {
             }
         }
 
-        //Else statement to do the opposite of the above if statement to be able to produce same results even when strings are swaped.
-        else {
-            for (int j = 0; j < stringTwo.length(); j++) {
-                for (int i = 0; i < stringOne.length(); i++) {
-                    if (stringTwoArray[j] == stringOneArray[i]) {
-                        System.out.println(stringTwoArray[j]);
-                    }
-                }
-            }
-        }
     }
     public static void main(String [] args){
         getCommonChar("computers", "house");
