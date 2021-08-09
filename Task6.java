3444e13ee2104e0f6de41f69c64c22f8e2c198a1
Berned <bernedndebe@gmail.com>
2021-08-09 23:30:21 +0200

    Simplify Task6

--- Task6.java
+++ Task6.java
@@ -1,34 +1,24 @@
 public class Task6 {
 
-
     static int getMaximumNumber(int numberOne, int numberTwo, int numberThree) {
 
         int results = 0;
-        if (numberOne > numberTwo && numberOne > numberThree) {
+        if (numberOne >= numberTwo && numberOne >= numberThree) {
             results = numberOne;
         }
-        if (numberTwo > numberOne && numberTwo > numberThree) {
+        if (numberTwo >= numberOne && numberTwo >= numberThree) {
             results = numberTwo;
         }
-        if (numberThree > numberOne && numberThree > numberTwo) {
+        if (numberThree >= numberOne && numberThree >= numberTwo) {
             results = numberThree;
         }
         if (numberOne == numberTwo && numberTwo == numberThree) {
             results = numberOne;
         }
-        if (numberOne < 0) {
-            results = Math.max(numberTwo, numberThree);
-        }
-        if (numberTwo < 0) {
-            results = Math.max(numberOne, numberThree);
-        }
-        if (numberThree < 0) {
-            results = Math.max(numberTwo, numberOne);
-        }
         return results;
     }
 
     public static void main(String[] args) {
-        System.out.print(getMaximumNumber(-74, -74, 74));
+        System.out.print(getMaximumNumber(73, 24, -74));
     }
 }
